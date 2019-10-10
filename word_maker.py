2f3f1a517a3f27c5be82305f112603f326761eca
FinalStryke <51430642+FinalStryke@users.noreply.github.com>
2019-10-10 16:18:39 +0900

    Add files via upload

---
+++ word_maker.py
@@ -0,0 +1,22 @@
+import random
+
+# Using Scrabble distributions, minus the vowels
+consonant = ["t", "t", "t", "t", "t", "t", "r", "r", "r", "r", "r", "r", "n",
+        "n", "n", "n", "n", "n", "s", "s", "s", "s", "l", "l", "l", "l", "d",
+        "d", "d", "d", "g", "g", "g", "g", "p", "p", "m", "m", "c", "c", "b",
+        "b", "y", "y", "w", "w", "v", "v", "h", "h", "f", "f", "k", "j", "x",
+        "q", "z"]
+
+# 50/50 a vowel is used at all
+vowel = ["", "", "", "", "", "a", "e", "i", "o", "u"]
+
+word = ""
+
+length = random.randint(3, 8)
+
+while length > 0:
+    word += random.choice(consonant)
+    word += random.choice(vowel)
+    length -= 1
+
+print(word.capitalize())
